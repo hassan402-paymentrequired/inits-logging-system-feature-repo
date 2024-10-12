3685dfa3b5c92b91f1da05af4ab16cfd70333c11
codeWithMe <codewithhassan.404.not.found@gmail.com>
2024-10-13 00:10:27 +0100

    implement the admin dashboard

--- app/Http/Controllers/Web/Admin/AdminController.php
+++ app/Http/Controllers/Web/Admin/AdminController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers\Web\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\StaffCheckIns;
+use App\Models\User;
+use App\Models\Visitor;
+use App\Models\VisitorHistories;
 use Illuminate\Http\Request;
 use Illuminate\Contracts\View\View;
 
@@ -15,7 +19,26 @@ class AdminController extends Controller
      */
     public function index(): View
     {
-        return view('dash'); //TODO: remove welcome
+        $month = date('m'); 
+        $year = date('Y');   
+        $currently_checked_in_staff_for_the_month = StaffCheckIns::whereYear('check_in_time', $year)
+            ->whereMonth('check_in_time', $month)
+            ->with('user')
+            ->get();
+
+            $currently_checked_in_visitor_for_the_month = VisitorHistories::whereYear('check_in_time', $year)
+            ->whereMonth('check_in_time', $month)
+            ->with(['visitor.user'])
+            ->get();
+
+        $currently_checked_in_visitors = VisitorHistories::whereDate('check_in_time', date('Y-m-d'))->with(['visitor.user'])->get();
+        $currently_checked_in_staff = StaffCheckIns::whereDate('check_in_time', date('Y-m-d'))->with('user')->get();
+        
+        return view('dash', [
+            'currently_checked_in_visitors' => $currently_checked_in_visitors,
+            'currently_checked_in_staff' => $currently_checked_in_staff,
+            'staff_for_the_month' => $currently_checked_in_staff_for_the_month,
+            'visitor_for_the_month' => $currently_checked_in_visitor_for_the_month
+        ]);
     }
-    
 }
